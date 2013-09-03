2586c0234f758ada5cc4a2e4e1e81fe2a79a911d
thomas <thomas@icontroller.eu>
2013-09-03 22:53:10 +0200

    Basic user/security setup

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,9 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
             new Scoreboard\AppBundle\ScoreboardAppBundle(),
+            new Scoreboard\UserBundle\ScoreboardUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Scoreboard/UserBundle/ScoreboardUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Scoreboard\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ScoreboardUserBundle extends Bundle
+{
+}
