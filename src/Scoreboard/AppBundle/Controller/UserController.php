6d6227a2b7943a7da6b743cb1bd1a51763d1276b
thomas <thomas@icontroller.eu>
2013-09-03 22:21:57 +0200

    Basic layout

---
+++ src/Scoreboard/AppBundle/Controller/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Scoreboard\AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class UserController extends Controller
+{
+    /**
+     * @Route("/login")
+     * @Template()
+     */
+    public function loginAction()
+    {
+        return array();
+    }
+}
